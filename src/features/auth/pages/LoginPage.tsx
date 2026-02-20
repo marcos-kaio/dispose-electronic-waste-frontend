0683052e2ceeff689577888d3ec2f50009124cc5
marcos-kaio <marcosb1@outlook.com>
2026-02-19 21:19:50 -0300

    feat(loginPage): adjust final UI details and responsiveness

--- src/features/auth/pages/LoginPage.tsx
+++ src/features/auth/pages/LoginPage.tsx
@@ -10,17 +10,17 @@ import { EyeOff } from "lucide-react";
 
 function LoginPage() {
   return (
-    <div className="flex flex-col w-full justify-center items-center">
-      <img src={logosImage} alt="" className="w-[40%] my-10" />
+    <div className="flex flex-col w-full min-h-screen justify-center items-center py-10 px-5">
+      <img src={logosImage} alt="" className="w-9/10 md:w-[40%] max-w-125. mb-8 md:mb-10" />
 
-      <h1 className="font-bold text-[32px] mb-10">
+      <h1 className="font-bold text-xl text-center md:text-[32px] mb-8 md:mb-10 max-w-300">
         Sistema de descarte de equipamentos de TIC e eletrônicos
       </h1>
 
-      <div className="flex flex-col items-center w-[35%] h-auto border-2 border-[#224185] rounded-[10px] p-9">
-        <h2 className="font-bold text-[36px] text-[#224185] mb-9">Login</h2>
+      <div className="flex flex-col items-center w-full max-w-112.5 h-auto border-2 border-[#224185] rounded-[10px] p-6 md:p-9 bg-white mb-5">
+        <h2 className="font-bold text-2xl md:text-[36px] text-[#224185] mb-6 md:mb-9">Login</h2>
 
-        <form className="flex flex-col gap-9 w-full px-4">
+        <form className="flex flex-col gap-6 md:gap-9 w-full">
           <Input
             className="w-full border border-[#224185] h-12.5"
             placeholder="Usuário"
@@ -33,9 +33,14 @@ function LoginPage() {
             </InputGroupAddon>
           </InputGroup>
 
-          <Button className="w-[70%] self-center bg-[#224185] color-white h-12.5 cursor-pointer" type="submit">
+          <Button className="w-full sm:w-[70%] self-center bg-[#224185] hover:bg-[#60749e] text-white text-sm sm:text-2xl h-12.5 cursor-pointer mb-2" type="submit">
             Acessar
           </Button>
+
+          <div className="flex flex-col items-center sm:flex-row gap-1 justify-center text-sm md:text-auto text-black font-light">
+            <p>Problemas com o acesso?</p>
+            <a href="" className="underline underline-offset-3 hover:decoration-[#6076A6]">Entre em contato com o CMI</a>
+          </div>
         </form>
       </div>
     </div>
